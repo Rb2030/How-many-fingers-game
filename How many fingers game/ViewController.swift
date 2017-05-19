908151cefad95c26d8cd6839628ee0c670e509cc
Rb2030 <rossmail2050@gmail.com>
2017-05-19 10:42:07 +0100

    initial commit

--- How many fingers game/ViewController.swift
+++ How many fingers game/ViewController.swift
@@ -9,7 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+   
+    @IBOutlet weak var fingerGuess: UITextField!
+    @IBAction func submittingButton(_ sender: Any) {
 
+    let randNum = String(arc4random_uniform(6))
+    
+    if fingerGuess.text == randNum(textfield) {
+    
+    yesNoResponce.text =  "Yes, you're right that's how many fingers I'm holding up!"
+    
+    } else {
+        
+        yesNoResponce.text = "Sorry that's not the number of fingers I am holding up! It was a " + randNum + "!"
+     }
+    
+    }
+    
+    @IBOutlet weak var yesNoResponse: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
